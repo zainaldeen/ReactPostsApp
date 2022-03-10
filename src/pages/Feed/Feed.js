b22861cbae247fb42e9df6222a8e92805f6db9b0
Zain AlDeen Fayod <zainaldeenfayod@gmail.com>
2022-03-10 11:30:10 -0500

    fix deletion

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -182,7 +182,10 @@ class Feed extends Component {
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
     fetch('http://localhost:8080/feeds/post/' + postId, {
-      method: 'DELETE'
+      method: 'DELETE',
+      headers: {
+        'Authorization': 'Bearer ' + this.props.token,
+      },
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
