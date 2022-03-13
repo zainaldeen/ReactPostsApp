8b19ea6cedab793795a8a6a6e888ae24ccf7442f
Zain AlDeen Fayod <zainaldeenfayod@gmail.com>
2022-03-13 09:08:59 -0400

    implement adding posts for socket io

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -56,6 +56,22 @@ class Feed extends Component {
   }
 
 
+  addPost = post => {
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      if (prevState.postPage === 1) {
+        if (prevState.posts.length >= 2) {
+          updatedPosts.pop();
+        }
+        updatedPosts.unshift(post);
+      }
+      return {
+        posts: updatedPosts,
+        totalPosts: prevState.totalPosts + 1
+      };
+    });
+  };
+
 
   loadPosts = direction => {
     if (direction) {
