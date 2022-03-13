a45b9cddb2aadfa3c7dd06a20533cfe19241d0e6
Zain AlDeen Fayod <zainaldeenfayod@gmail.com>
2022-03-13 12:25:22 -0400

    fix doublicat the posts twice

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -85,6 +85,8 @@ class Feed extends Component {
     });
   };
 
+
+
   loadPosts = direction => {
     if (direction) {
       this.setState({ postsLoading: true, posts: [] });
@@ -200,9 +202,7 @@ class Feed extends Component {
               p => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
-          } else if (prevState.posts.length < 2) {
-            updatedPosts = prevState.posts.concat(post);
-          }
+          } 
           return {
             posts: updatedPosts,
             isEditing: false,
